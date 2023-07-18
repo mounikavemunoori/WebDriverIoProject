ba31f97e3ac932daf2e666a80df010d98e851c16
mounikavemunoori <mounikavemunoori@gmail.com>
2023-07-18 13:02:27 +0530

    updated the package json for reports

--- wdio.conf.js
+++ wdio.conf.js
@@ -123,7 +123,16 @@ export const config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: ['spec',
+    [
+      'cucumberjs-json',
+      {
+        jsonFolder: './reports/json',
+        language: 'en',
+      },
+    ],
+  ],
+    
 
     //
     // If you are using Cucumber you need to specify the location of your step definitions.
